ec94fa8f8d6f16263eaf6a88cf9f2caf030d17c4
Stanley Bileschi <bileschi@google.com>
2018-07-27 17:00:52 -0400

    refactor tests for clarity

--- src/engine/preprocessing_training_test.ts
+++ src/engine/preprocessing_training_test.ts
@@ -2,14 +2,14 @@
 
 
 // tslint:disable-next-line:max-line-length
-import {Tensor, tensor2d, test_util, randomNormal} from '@tensorflow/tfjs-core';
+import {randomNormal, Tensor, tensor2d, test_util} from '@tensorflow/tfjs-core';
 import {expectValuesInRange} from '@tensorflow/tfjs-core/dist/test_util';
 
 import * as tfl from '../index';
 import {initializers} from '../index';
 import {getInitializer} from '../initializers';
 // tslint:disable-next-line:max-line-length
-import {VocabLayerOptimizer, UnitVarianceOptimizer, ZeroMeanOptimizer} from '../preprocess-layers/preprocess_core';
+import {UnitVarianceOptimizer, VocabLayerOptimizer, ZeroMeanOptimizer} from '../preprocess-layers/preprocess_core';
 import {describeMathCPU, describeMathCPUAndGPU} from '../utils/test_utils';
 import {expectTensorsClose} from '../utils/test_utils';
 
@@ -98,60 +98,49 @@ describeMathCPU('String Preproc Model.fit', () => {
     const trainInputs = tfl.preprocessing.stringTensor2d(
         [['a', 'a'], ['b', 'b'], ['c', 'c'], ['d', 'd']]);
     // Fit the model to a tensor of strings.
-    await vocabModel.fit(trainInputs, null, {batchSize: 1, epochs: 1})
-        .then(history => {
-          const testInputs = tfl.preprocessing.stringTensor2d(
-              [['a', 'b'], ['c', 'd'], ['hello', 'world']]);
-          const testOutputs = vocabModel.predict(testInputs);
-          test_util.expectArraysClose(
-              testOutputs as Tensor,
-              tensor2d([[0, 1], [2, 3], [4, 4]], [3, 2], 'int32'));
-          done();
-        })
-        .catch(err => {
-          done.fail(err.stack);
-        });
+    await vocabModel.fit(trainInputs, null, {batchSize: 1, epochs: 1});
+    const testInputs = tfl.preprocessing.stringTensor2d(
+        [['a', 'b'], ['c', 'd'], ['hello', 'world']]);
+    const testOutputs = vocabModel.predict(testInputs);
+    test_util.expectArraysClose(
+        testOutputs as Tensor,
+        tensor2d([[0, 1], [2, 3], [4, 4]], [3, 2], 'int32'));
+    done();
   });
 
-  it('Fit vocab layer overrides initializer',
-      async done => {
-        // Define a Sequential model with just one layer:  Vocabulary.
-        const vocabModel = tfl.sequential({
-          layers: [tfl.layers.vocab({
-            name: 'myVocabLayer',
-            knownVocabSize: 4,
-            hashVocabSize: 1,
-            vocabInitializer: getInitializer({
-              className: 'KnownVocab',
-              config: {strings: ['hello', 'world', 'こんにちは', '世界']}
-            }),
-            // TODO(bileschi): Use tfl.preprocessing.vocabLayerOptimizer instead
-            // of direct access.
-            optimizer: new VocabLayerOptimizer(),
-            inputShape: [2]  // two words per example
-          })]
-        });
-        // Compile the model.
-        // TODO(bileschi): It should be possible to compile with null / null
-        // here.
-        vocabModel.compile({optimizer: 'SGD', loss: 'meanSquaredError'});
-        const trainInputs = tfl.preprocessing.stringTensor2d(
-            [['a', 'a'], ['a', 'a'], ['a', 'a'], ['a', 'a']]);
-        // Fit the model to a tensor of strings.
-        await vocabModel.fit(trainInputs, null, {batchSize: 1, epochs: 1})
-            .then(history => {
-              const testInputs = tfl.preprocessing.stringTensor2d(
-                  [['a', 'a'], ['a', 'a'], ['hello', 'world']]);
-              const testOutputs = vocabModel.predict(testInputs);
-              test_util.expectArraysClose(
-                  testOutputs as Tensor,
-                  tensor2d([[0, 0], [0, 0], [4, 4]], [3, 2], 'int32'));
-              done();
-            })
-            .catch(err => {
-              done.fail(err.stack);
-            });
-      });
+  it('Fit vocab layer overrides initializer', async done => {
+    // Define a Sequential model with just one layer:  Vocabulary.
+    const vocabModel = tfl.sequential({
+      layers: [tfl.layers.vocab({
+        name: 'myVocabLayer',
+        knownVocabSize: 4,
+        hashVocabSize: 1,
+        vocabInitializer: getInitializer({
+          className: 'KnownVocab',
+          config: {strings: ['hello', 'world', 'こんにちは', '世界']}
+        }),
+        // TODO(bileschi): Use tfl.preprocessing.vocabLayerOptimizer instead
+        // of direct access.
+        optimizer: new VocabLayerOptimizer(),
+        inputShape: [2]  // two words per example
+      })]
+    });
+    // Compile the model.
+    // TODO(bileschi): It should be possible to compile with null / null
+    // here.
+    vocabModel.compile({optimizer: 'SGD', loss: 'meanSquaredError'});
+    const trainInputs = tfl.preprocessing.stringTensor2d(
+        [['a', 'a'], ['a', 'a'], ['a', 'a'], ['a', 'a']]);
+    // Fit the model to a tensor of strings.
+    await vocabModel.fit(trainInputs, null, {batchSize: 1, epochs: 1});
+    const testInputs = tfl.preprocessing.stringTensor2d(
+        [['a', 'a'], ['a', 'a'], ['hello', 'world']]);
+    const testOutputs = vocabModel.predict(testInputs);
+    test_util.expectArraysClose(
+        testOutputs as Tensor,
+        tensor2d([[0, 0], [0, 0], [4, 4]], [3, 2], 'int32'));
+    done();
+  });
 });
 
 
@@ -159,13 +148,13 @@ describeMathCPU('Preprocess with zeroMean & unitVariance', () => {
   it('Fit a model with two preprocessing layers.', async done => {
     // Define a Sequential model with zeroMean & unitVariance
     const normalizationModel = tfl.sequential({
-      layers: [tfl.layers.zeroMean({
-        optimizer: new ZeroMeanOptimizer(),
-        inputShape: [3]  // 3 numbers per example
-      }),
-      tfl.layers.unitVariance({
-          optimizer: new UnitVarianceOptimizer()
-      })]
+      layers: [
+        tfl.layers.zeroMean({
+          optimizer: new ZeroMeanOptimizer(),
+          inputShape: [3]  // 3 numbers per example
+        }),
+        tfl.layers.unitVariance({optimizer: new UnitVarianceOptimizer()})
+      ]
     });
     // Compile the model.
     // TODO(bileschi): It should be possible to compile with null / null
@@ -176,19 +165,12 @@ describeMathCPU('Preprocess with zeroMean & unitVariance', () => {
     const std = 13;
     const trainInputs = randomNormal([100, 3], mean, std);
     // Fit the model to the provided samples.
-    await normalizationModel.fit(trainInputs, null, {batchSize: 10, epochs: 1})
-      .then(history => {
-        const testInputs = tensor2d(
-          [[mean - std, mean, mean + std]]);
-        const testOutputs = normalizationModel.predict(testInputs);
-        // Expect an accurate prediction of the stddev (within 1%)
-            test_util.expectArraysClose(
-              testOutputs as Tensor,
-              tensor2d([[-1, 0, 1]], [1, 3], 'float32'), 0.5);
-              done();
-      })
-      .catch(err => {
-        done.fail(err.stack);
-      });
+    await normalizationModel.fit(trainInputs, null, {batchSize: 10, epochs: 1});
+    const testInputs = tensor2d([[mean - std, mean, mean + std]]);
+    const testOutputs = normalizationModel.predict(testInputs);
+    // Expect an accurate prediction of the stddev (within 1%)
+    test_util.expectArraysClose(
+        testOutputs as Tensor, tensor2d([[-1, 0, 1]], [1, 3], 'float32'), 0.5);
+    done();
   });
 });
